5fb20b47fbbd12ce70c3a3e938b986a0ea669588
lokeshwaranm <mlokesh672000@gmail.com>
2022-02-22 19:04:45 +0530

    csv file write finish and process minimum,maximum,average.

--- src/main/scala/LogAnalyzer.scala
+++ src/main/scala/LogAnalyzer.scala
@@ -1,46 +1,61 @@
 import com.typesafe.scalalogging.LazyLogging
 import org.apache.commons.lang3.time.StopWatch
 
+import java.io.PrintWriter
+import scala.collection.mutable.ArrayBuffer
+
 class LogAnalyzer extends ProcessLogFile with LazyLogging {
 
   /** given the log file path and performing the operation and return the csv file.
     * @param logFilePath give the log file path.
     */
-  def analyzeLogFile(logFilePath: String): String = {
+  def analyzeLogFile(
+      logFilePath: String
+  ): Any = {
     val stopWatch = new StopWatch()
     stopWatch.start()
     logger.info("analyzeLogFile()::entry")
-    val fileExtension = checkFileExtension(filePath = logFilePath)
-    val readFile = readLogFile(logFilePath)
-    val jobPatternMatch = jobPatternMatching(readFile)
+    val isFileExtension = checkFileExtension(logFilePath)
+    if (isFileExtension) {
+      val contentFile = readLogFile(logFilePath)
+      val jobPatternMatch = jobPatternMatching(contentFile)
+      val uniqueStr = strUniqueValue(jobPatternMatch)
+      val processMin = processMinValue(jobPatternMatch)
+      val processMax = processMaxValue(jobPatternMatch)
+      val processMean = processMeanValue(jobPatternMatch)
+      val csvFileConvert =
+        summariseLogContent(uniqueStr, processMin, processMax, processMean)
+      contentFile match {
+        case "file not found given path." =>
+          return "file not found given path."
+        case "Exception the read log file." =>
+          return "Exception the read log file."
+        case _ =>
+          jobPatternMatch.map(x => x._1).mkString("") match {
+            case "Process name and time length is not same." =>
+              return "Process name and time length is not same."
+            case _ => uniqueStr
+
+          }
+      }
+    } else {
+      return "Invalid log file or file path"
+    }
 //    if (fileExtension) {
-//      readLogFile(logFilePath) match {
-//        case "file not found given path." =>
-//          return "file not found given path."
-//        case "Exception the read log file." => "Exception the read log file."
-//        case _                              => return "working"
-//      }
-//    } else {
-//      return "Invalid log file or file path"
-//    }
-//    if (fileExtension) {
-////      Thread.sleep(12000)
 //      stopWatch.stop()
 //      logger.info(
 //        "analyzeLogFile()::normal_exit | " + stopWatch
 //          .getTime() + " ms "
 //      )
-//      return "valid"
 //    } else {
 //      stopWatch.stop()
 //      logger.debug(
 //        "analyzeLogFile()::normal_exit | " + stopWatch
 //          .getTime() + " ms "
 //      )
-//      "Invalid log file or file path"
+//      return "Invalid log file or file path"
 //    }
-//    readFile
-    jobPatternMatch
+    csvFileConvert
   }
 
   /** Given the unique Process of task name and Process of task taking time of minimum,maximum and average the data write to csv file.
@@ -50,19 +65,62 @@ class LogAnalyzer extends ProcessLogFile with LazyLogging {
     * @param meanValue process of task taking total time of meanValue.
     */
   def summariseLogContent(
-      uniqueStr: Array[String],
-      miniValue: Array[Double],
-      maxValue: Array[Double],
-      meanValue: Array[Double]
-  ): Unit = {}
+      uniqueStr: ArrayBuffer[String],
+      miniValue: ArrayBuffer[Int],
+      maxValue: ArrayBuffer[Int],
+      meanValue: ArrayBuffer[Double]
+  ): Any = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info(
+      "summariseLogContent(uniqueStr,miniValue,maxValue,meanValue)::entry"
+    )
+    val csvFileCreate = new PrintWriter(
+      "logFile/log-analyzer.csv"
+    )
+    var count = 1
+    var strCSV = ""
+    try {
+      for (i <- uniqueStr.indices) {
+        strCSV +=
+          count + "\t" + "PROCESS NAME : " + "\t" + uniqueStr(i) + "\n" + "\t" +
+            "PROCESS TAKING MINIMUM TIME : " + "\t" + miniValue(
+              i
+            ) + "\n" + "\t" +
+            "PROCESS TAKING MAXIMUM TIME : " + "\t" + maxValue(
+              i
+            ) + "\n" + "\t" +
+            "PROCESS TAKING AVERAGE TIME : " + "\t" + meanValue(i) + "\n\n"
+        count += 1
+      }
+    } catch {
+      case a: ArrayIndexOutOfBoundsException =>
+        stopWatch.stop()
+        logger.error(
+          "summariseLogContent(uniqueStr,miniValue,maxValue,meanValue)::error | " + stopWatch
+            .getTime() + " ms "
+        )
+        return "Values of Data Index is mismatch."
+    }
+    csvFileCreate.write(strCSV)
+    csvFileCreate.close()
+    stopWatch.stop()
+    logger.info(
+      "summariseLogContent(uniqueStr,miniValue,maxValue,meanValue)::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    csvFileCreate
+  }
 }
 object LogAnalyzerObject extends App {
   val logAnalyzerObject = new LogAnalyzer
   println(
     logAnalyzerObject
       .analyzeLogFile(logFilePath = "logFile/expert-system.log")
+//      ._1
+//      .length
 //      .mkString("Array(", ", ", ")")
-      .length
+//      .length
   )
 
 }
--- src/main/scala/ProcessLogFile.scala
+++ src/main/scala/ProcessLogFile.scala
@@ -1,71 +1,238 @@
+import org.apache.commons.lang3.time.StopWatch
+
 import java.io.FileNotFoundException
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.reflect.io.File
 import scala.io.Source.fromFile
-import scala.util.matching.Regex
+import com.typesafe.scalalogging.LazyLogging
 
-class ProcessLogFile {
+class ProcessLogFile extends LazyLogging {
 
   /** Given the file path and check the file extension.
     * @param filePath give the log file path.
     */
   def checkFileExtension(filePath: String): Boolean = {
+    logger.info("checkFileExtension(filePath)::entry")
+    val stopWatch = new StopWatch()
+    stopWatch.start()
     val file =
       File(filePath).exists
     if (file)
-      if (filePath.endsWith(".log"))
+      if (filePath.endsWith(".log")) {
+        stopWatch.stop()
+        logger.info(
+          "checkFileExtension(filePath)::normal_exit | " + stopWatch
+            .getTime() + " ms "
+        )
         return true
+      }
+    stopWatch.stop()
+    logger.info(
+      "checkFileExtension(filePath)::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
     false
-//    filePath.endsWith(".log")
-//    file
   }
 
   /** read that log file.
     * @param filePath give the log file path.
     */
   protected def readLogFile(filePath: String): String = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("readLogFile(filePath)::entry")
+    stopWatch.stop()
     try {
+      logger.info(
+        "readLogFile(filePath)::normal_exit | " + stopWatch
+          .getTime() + " ms "
+      )
       fromFile(filePath).mkString("")
     } catch {
-      case f: FileNotFoundException => s"file not found given path."
-      case e: Exception             => "Exception the read log file."
+      case f: FileNotFoundException =>
+        logger.error("readLogFile :: error | " + stopWatch.getTime() + " ms ")
+        "file not found given path."
+      case e: Exception =>
+        logger.error("readLogFile :: error | " + stopWatch.getTime() + " ms ")
+        "Exception the read log file."
     }
   }
 
   /** process of task name and time matching.
     * @param strLogContent give the string of log content.
     */
-  def jobPatternMatching(strLogContent: String): String = {
+  protected def jobPatternMatching(
+      strLogContent: String
+  ): ListBuffer[(String, Int)] = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("jobPatternMatching(strLogContent)::entry")
+    var processNameTime = new ListBuffer[(String, Int)]()
     val jobPatternMatchingRegex =
-      """[\w]+[(]([\w \-_,/]+)?[)]+[:]+[\w]+[ |]+[0-9]+[msMS ]+""".r
-    val processNameMatchingRegex = """[\w]+[(]([\w \-_,/]+)?[)]+""".r
-    val processTimeMatchingRegex = """[0-9 ]+[msMS]+""".r
+      """[\w]+[(]([\w \-_,/=]+)?[)]+[:]+[\w]+[ |]+[0-9]+[ms ]+""".r
+    val processNameMatchingRegex = """[\w]+[(]([\w \-_,/=]+)?[)]+""".r
+    val processTimeMatchingRegex = """([0-9]+[ ms])""".r
+    val processTimeRegex = """[0-9]+""".r
 
     val fullLineMatchString = jobPatternMatchingRegex
       .findAllMatchIn(strLogContent)
       .mkString("")
-    processNameMatchingRegex.findAllMatchIn(fullLineMatchString).mkString("")
-    processTimeMatchingRegex.findAllMatchIn(fullLineMatchString).mkString("")
-//      .mkString("") //.toArray
+
+    val processName =
+      processNameMatchingRegex.findAllMatchIn(fullLineMatchString).toArray
+
+    val processTimes = processTimeRegex
+      .findAllMatchIn(
+        processTimeMatchingRegex
+          .findAllMatchIn(fullLineMatchString)
+          .mkString("")
+      )
+      .toArray
+    try {
+      for (i <- processName.indices) {
+        processNameTime += (
+          (
+            processName(i).toString(),
+            processTimes(i).toString().toInt
+          )
+        )
+      }
+    } catch {
+      case a: ArrayIndexOutOfBoundsException =>
+        stopWatch.stop()
+        logger.error(
+          "jobPatternMatching::error | " + stopWatch.getTime() + " ms "
+        )
+        processNameTime.clear()
+        return processNameTime += (
+          (
+            "Process name and time length is not same.",
+            1
+          )
+        )
+    }
+    stopWatch.stop()
+    logger.info(
+      "jobPatternMatching(strLogContent)::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    processNameTime
   }
 
   /** process of task names return the unique process of task names.
     * @param matchString process of task names and times.
     */
-  def strUniqueValue(matchString: ListBuffer[(String, Int)]) = {}
+  protected def strUniqueValue(
+      matchString: ListBuffer[(String, Int)]
+  ): ArrayBuffer[String] = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("strUniqueValue(ListBuffer[(String,Int)])::entry")
+    val processName = matchString.map(x => x._1)
+    val uniqueNameArray = new ArrayBuffer[String]()
+    for (i <- processName.indices) {
+      if (!(uniqueNameArray.contains(processName(i)))) {
+        uniqueNameArray += processName(i)
+      }
+    }
+    stopWatch.stop()
+    logger.info(
+      "strUniqueValue(ListBuffer[(String,Int)])::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    uniqueNameArray
+  }
 
   /** process of task taking minimum time.
     * @param matchString process of task names and times.
     */
-  def processMinValue(matchString: ListBuffer[(String, Int)]) = {}
+  protected def processMinValue(
+      matchString: ListBuffer[(String, Int)]
+  ): ArrayBuffer[Int] = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("processMinValue(ListBuffer[(String,Int)])::entry")
+    val uniqueStr = strUniqueValue(matchString)
+    var minValues = new ArrayBuffer[Int]()
+    var collection = new ArrayBuffer[Int]()
+    uniqueStr.foreach { i => //compare uniqueStr and matchString.
+      var uniStr = i
+      matchString.foreach { j =>
+        var matchCurrentString = j._1
+        if (uniStr == matchCurrentString) {
+          collection += j._2
+        }
+      }
+      minValues += collection.min
+      collection.clear()
+    }
+    stopWatch.stop()
+    logger.info(
+      "processMinValue(ListBuffer[(String,Int)])::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    minValues
+  }
 
   /** process of task taking maximum time.
     * @param matchString process of task names and times.
     */
-  def processMaxValue(matchString: ListBuffer[(String, Int)]) = {}
+  protected def processMaxValue(
+      matchString: ListBuffer[(String, Int)]
+  ): ArrayBuffer[Int] = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("processMaxValue(ListBuffer[(String,Int)])::entry")
+    val uniqueStr = strUniqueValue(matchString)
+    var maxValues = new ArrayBuffer[Int]()
+    var collection = new ArrayBuffer[Int]()
+    uniqueStr.foreach { i => //compare uniqueStr and matchString.
+      var uniStr = i
+      matchString.foreach { j =>
+        var matchCurrentString = j._1
+        if (uniStr == matchCurrentString) {
+          collection += j._2
+        }
+      }
+      maxValues += collection.max
+      collection.clear()
+    }
+    stopWatch.stop()
+    logger.info(
+      "processMaxValue(ListBuffer[(String,Int)])::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    maxValues
+  }
 
   /** process of task taking average time.
     * @param matchString process of task name and times.
     */
-  def processMeanValue(matchString: ListBuffer[(String, Int)]) = {}
+  protected def processMeanValue(
+      matchString: ListBuffer[(String, Int)]
+  ): ArrayBuffer[Double] = {
+    val stopWatch = new StopWatch()
+    stopWatch.start()
+    logger.info("processMeanValue(ListBuffer[(String,Int)])::entry")
+    val uniqueStr = strUniqueValue(matchString)
+    var meanValues = new ArrayBuffer[Double]()
+    var collection = new ArrayBuffer[Int]()
+    uniqueStr.foreach { i => //compare uniqueStr and matchString.
+      var uniStr = i
+      matchString.foreach { j =>
+        var matchCurrentString = j._1
+        if (uniStr == matchCurrentString) {
+          collection += j._2
+        }
+      }
+      meanValues += ((collection.sum).toDouble / collection.length.toDouble)
+      collection.clear()
+    }
+    stopWatch.stop()
+    logger.info(
+      "processMeanValue(ListBuffer[(String,Int)])::normal_exit | " + stopWatch
+        .getTime() + " ms "
+    )
+    meanValues
+  }
 }
